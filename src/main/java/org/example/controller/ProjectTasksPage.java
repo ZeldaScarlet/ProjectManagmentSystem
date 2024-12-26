eb0d3cc635f2891b62ecc515b5289edf8c93b3fd
kaanaydin-0 <mustafakaanaydin6@gmail.com>
2024-12-26 19:24:24 +0300

    branch masterdan main olarak ayarlandı

--- src/main/java/org/example/controller/ProjectTasksPage.java
+++ src/main/java/org/example/controller/ProjectTasksPage.java
@@ -58,7 +58,7 @@ public class ProjectTasksPage extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 onBack.run();
-            }
+            }.
         });
 
         bottomPanel.add(addTaskButton);
